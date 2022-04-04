b9095ab3728e061b544d68ad0a7af81f40fb3c36
BrunoPorras <alefran2020@gmail.com>
2022-04-04 00:17:28 -0500

    Added list of items

--- screens/ActivateAccountScreen2.js
+++ screens/ActivateAccountScreen2.js
@@ -1,16 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { View, Text, Button, StyleSheet, TextInput, Image } from 'react-native'
+import { ListItem } from 'react-native-elements'
 import { useNavigation } from '@react-navigation/native'
 import NewButton from '../components/buttons'
 
 import mailIcon from '../assets/mail.png'
 import phoneIcon from '../assets/phone.png'
 
+const log = () => console.log('this is an example method');
+
 const ActivateAccountScreen2 = () => {
     const navigation = useNavigation()
     const handleNext = () => {
         navigation.navigate("ActivatedAccountScreen")
     }
+
+    const sedes = [{
+        name: "Cangallo"
+    }, {
+        name: "Ciudad universitaria"
+    }]
+
+    const [sede, setSede] = useState("Sede de preferencia")
+
+    const [expanded, setExpanded] = useState(false)
+
     return (
         <View style={styles.container}>
             <Text style={styles.tittle}>
@@ -35,9 +49,30 @@ const ActivateAccountScreen2 = () => {
                 </Image>
             </View>
             <View style={styles.inputDropContainer}>
-                <TextInput
-                    placeholder='Sede preferencia'>
-                </TextInput>
+                <ListItem.Accordion
+                    containerStyle={styles.listItem}
+                    content={
+                        <>
+                            <ListItem.Content style={styles.listItemTittle}>
+                                <ListItem.Title style={styles.listItemTittle}>
+                                    {sede}
+                                </ListItem.Title>
+                            </ListItem.Content>
+                        </>
+                    }
+                    isExpanded={expanded}
+                    onPress={() => {
+                        setExpanded(!expanded);
+                    }}>
+                    {sedes.map((sede) => (
+                        <ListItem key={sede.name} onPress={(e)=> setSede(sede.name)} bottomDivider
+                        containerStyle={styles.listItem}>
+                            <ListItem.Content>
+                                <ListItem.Title>{sede.name}</ListItem.Title>
+                            </ListItem.Content>
+                        </ListItem>
+                    ))}
+                </ListItem.Accordion>
             </View>
             <NewButton
                 width_="100%"
@@ -80,10 +115,17 @@ const styles = StyleSheet.create({
         width: 15,
         height: 25
     },
+    inputDropContainer: {
+        marginBottom: 10 
+    },
     listContainer: {
-        
         borderBottomColor: "#B2B2B2",
-
+    },
+    listItem: {
+        backgroundColor: "#F2F2F2"
+    },
+    listItemTittle: {
+        color: "#B2B2B2"
     }
 });
 
